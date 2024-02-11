dbbbd2444065bb2f11524ceb962fec2c5eb1df33
Vinicius Abreu <vs.abreu@hotmail.com>
2024-02-11 06:47:35 -0300

    fix: use connectionstring postgres

--- src/database/postgres.ts
+++ src/database/postgres.ts
@@ -2,11 +2,7 @@ import { Pool } from "pg";
 import { env } from "../utils/env";
 
 export const postgres = new Pool({
-  user: env.PG_USER,
-  host: env.PG_HOST,
-  database: env.PG_DB,
-  password: env.PG_PASS,
-  port: env.PG_PORT,
-  max: 10,
+  connectionString: `postgres://${env.PG_USER}:${env.PG_PASS}:${env.PG_PORT}/${env.PG_DB}`,
+  max: 15,
   idleTimeoutMillis: 7000,
 });
